725285633ce078a7f3ae9ddd535ec259efdb4b9c
Wang Shenghao <shenghao.wang@gojek.com>
2023-04-07 16:39:01 +0800

    Export data to file

--- src/51job.py
+++ src/51job.py
@@ -1,6 +1,9 @@
+from pathlib import Path
 from typing import Dict
 
+import pandas as pd
 from loguru import logger
+from omegaconf import DictConfig
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -8,8 +11,16 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
+import hydra
 
-def main(url):
+
+@hydra.main(version_base=None, config_path="hydra", config_name="config")
+def main(cfg: DictConfig):
+
+    output_dir = Path.cwd() / "51job_output"
+    output_dir.mkdir(exist_ok=True)
+
+    datadir = Path(hydra.utils.get_original_cwd()) / cfg.datadir
 
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
@@ -17,9 +28,9 @@ def main(url):
         options=options, service=Service(ChromeDriverManager().install())
     )
 
-    # url = "https://shenghaowang.github.io/"
-    driver.get(url)
+    driver.get(cfg.url)
     logger.info(driver.title)
+
     try:
         WebDriverWait(driver, 30).until(
             EC.presence_of_element_located((By.CLASS_NAME, "j_joblist"))
@@ -28,7 +39,7 @@ def main(url):
         jobs_container = driver.find_elements(By.CLASS_NAME, "j_joblist")
 
         if not jobs_container:
-            logger.info("No job is available.")
+            logger.warning("No job is available.")
 
         jobs_container = jobs_container[0]
 
@@ -37,33 +48,35 @@ def main(url):
         jobs = jobs_container.find_elements(By.CLASS_NAME, "sensors_exposure")
         logger.info(f"Number of jobs: {len(jobs)}")
 
+        job_specs = []
         for job in jobs:
-            job_specs = extract_job_info(job)
-            logger.debug(job_specs)
+            job_obj = extract_job_info(job)
+            job_specs.append(job_obj)
+
+            logger.debug(job_obj)
 
     finally:
         driver.quit()
+
+    df = pd.DataFrame(job_specs)
+    df.to_csv(datadir / f"{cfg.keyword}.csv", index=False)
+
+    logger.info("Job completed!")
 
 
 def extract_job_info(job) -> Dict[str, str]:
     job_specs = {}
 
-    jname = job.find_element(By.CLASS_NAME, "jname").text
-    cname = job.find_element(By.CLASS_NAME, "cname").text
-    time = job.find_element(By.CLASS_NAME, "time").text
-    salary = job.find_element(By.CLASS_NAME, "sal").text
-    # logger.debug(f"{jname} - {cname} - {time} - {salary}")
-    job_specs["job_title"] = jname
-    job_specs["companny_name"] = cname
-    job_specs["post_date"] = time
-    job_specs["salary"] = salary
+    job_specs["job_title"] = job.find_element(By.CLASS_NAME, "jname").text
+    job_specs["companny_name"] = job.find_element(By.CLASS_NAME, "cname").text
+    job_specs["post_date"] = job.find_element(By.CLASS_NAME, "time").text
+    job_specs["salary"] = job.find_element(By.CLASS_NAME, "sal").text
 
     job_requirements = ""
     requirement_container = job.find_element(By.XPATH, "//span[@class='d at']")
     for requirement in requirement_container.find_elements(By.TAG_NAME, "span"):
         job_requirements += requirement.text
 
-    # logger.debug(f"job requirement: {job_requirements}")
     job_specs["requirements"] = job_requirements
 
     job_tags = []
@@ -71,29 +84,16 @@ def extract_job_info(job) -> Dict[str, str]:
     for tag in tags_container.find_elements(By.TAG_NAME, "span"):
         job_tags.append(tag.get_attribute("title"))
 
-    # logger.debug(f"Tags: {'|'.join(job_tags)}")
-    job_specs["tags"] = job_tags
+    job_specs["tags"] = "|".join(job_tags)
 
     size_container = job.find_element(By.XPATH, "//p[@class='dc at']")
-    firm_size = size_container.text
-    # logger.debug(f"Firm size: {firm_size}")
-    job_specs["firm_size"] = firm_size
+    job_specs["firm_size"] = size_container.text
 
     industry_container = job.find_element(By.XPATH, "//p[@class='int at']")
-    industry = industry_container.text
-    # logger.debug(f"industry: {industry}")
-    job_specs["industry"] = industry
+    job_specs["industry"] = industry_container.text
 
     return job_specs
 
 
 if __name__ == "__main__":
-    keyword = "化学工程师"
-    url = f"https://we.51job.com/pc/search?keyword={keyword}&searchType=2&sortType=0&metro="  # noqa: E501
-
-    # keyword = "python"
-    # page_no = 1
-
-    main(url)
-
-    logger.info("Job completed!")
+    main()
